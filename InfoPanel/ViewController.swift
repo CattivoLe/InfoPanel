380bd67252f880bc16ae11385afff4e406e394ad
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-03-30 23:26:57 +0300

    Initial Commit

---
+++ InfoPanel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfoPanel
+//
+//  Created by Alexander Omelchuk on 30/03/2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
