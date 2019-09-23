bc053c3b7defdd73c5a969e79186dccc69b44b1d
Alexander Omelchuk <cattivole@icloud.com>
2019-09-23 14:59:26 +0300

    Переделал TableView

--- InfoPanelExtension/ShareViewController.swift
+++ InfoPanelExtension/ShareViewController.swift
@@ -7,48 +7,51 @@
 //
 
 import UIKit
-import MobileCoreServices
-import CloudKit
 
 class ShareViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var tableView: UITableView! = UITableView()
-    let cellIdentifier = "Cell"
+    var tableView = UITableView()
+    let testArray = ["Test cell 1", "Test cell 2", "Test cell 3","Test cell 4"]
     
-    
-    //MARK: - Жизненный цикл контроллера
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super .viewWillAppear(animated)
-    }
+    //MARK: - View did Load
     
     override func viewDidLoad() {
         super .viewDidLoad()
         setupTableView()
     }
     
-    
-    //MARK: - TableView Deligate, DataSourse
+    //MARK: - TableVIew Delegate DataSourse
     
     private func setupTableView() {
+        let width = view.bounds.width
+        let height = view.bounds.height
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: width, height: height), style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.backgroundColor = .black
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        view.addSubview(tableView)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return testArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = .black
         cell.textLabel?.textColor = .white
+        cell.textLabel?.text = testArray[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        print(testArray[indexPath.row])
     }
+    
+    
+    
+    
     
 }
