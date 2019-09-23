8820c55690266500d767724e4cf56187242c0e48
Alexander Omelchuk <cattivole@icloud.com>
2019-09-23 12:42:17 +0300

    TableView

--- InfoPanelExtension/ShareViewController.swift
+++ InfoPanelExtension/ShareViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 import MobileCoreServices
 import CloudKit
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView! = UITableView()
+    let cellIdentifier = "Cell"
+    
+    
+    //MARK: - Жизненный цикл контроллера
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        setupTableView()
+    }
+    
+    
+    //MARK: - TableView Deligate, DataSourse
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.backgroundColor = .black
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
